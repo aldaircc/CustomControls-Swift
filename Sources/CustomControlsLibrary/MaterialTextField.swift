093a383540e6d4e16674cbfe094edfbda9e2050c
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-06-13 20:12:19 -0500

    base file for material textfield.

---
+++ Sources/CustomControlsLibrary/MaterialTextField.swift
@@ -0,0 +1,19 @@
+//
+//  MaterialTextField.swift
+//  
+//
+//  Created by Aldair Cosetito Coral on 13/06/22.
+//
+
+import UIKit
+
+public class MaterialTextField: UIView {
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+}
