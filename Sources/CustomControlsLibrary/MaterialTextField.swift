5ab0fe271744c4030f884ddecbd28fd74f523fbb
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-06-14 13:05:56 -0500

    Testing font size 13

--- Sources/CustomControlsLibrary/MaterialTextField.swift
+++ Sources/CustomControlsLibrary/MaterialTextField.swift
@@ -98,7 +98,7 @@ public class MaterialTextField: UIView {
             
             UIView.animate(withDuration: 0.3) {
                 self.placeHolder.frame.origin = destination
-                self.placeHolder.font = UIFont.systemFont(ofSize: 6) //reducir tamaño de letra
+                self.placeHolder.font = UIFont.systemFont(ofSize: 13) //reducir tamaño de letra
                 self.placeHolder.textColor = .red
                 print("Nuevo origin: \(self.placeHolder.frame.origin)")
             }
