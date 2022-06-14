acaaff8648270317a4705063806a880dfaa6a55f
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-06-13 20:13:00 -0500

    Setting variables and control views.

--- Sources/CustomControlsLibrary/MaterialTextField.swift
+++ Sources/CustomControlsLibrary/MaterialTextField.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 public class MaterialTextField: UIView {
+    // MARK: - Controls
+    lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    lazy var placeHolder: UILabel = {
+        let placeHolder = UILabel()
+        placeHolder.translatesAutoresizingMaskIntoConstraints = false
+        return placeHolder
+    }()
+    
+    lazy var activationIndicator: UIView = {
+        let activationIndicator = UIView()
+        activationIndicator.translatesAutoresizingMaskIntoConstraints = false
+        return activationIndicator
+    }()
+    
+    let placeHolderText: String
+    let activationColor: UIColor
     
     /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
 }
