64863363506b66f66e414963a40a984ac2f94734
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-06-14 13:45:17 -0500

    test

--- Sources/CustomControlsLibrary/MaterialTextField.swift
+++ Sources/CustomControlsLibrary/MaterialTextField.swift
@@ -101,8 +101,8 @@ public class MaterialTextField: UIView {
                 self.placeHolder.center.y = 5 //self.bounds.height + self.placeHolder.bounds.height
 //                self.placeHolder.font = UIFont.systemFont(ofSize: 13) //reducir tamaño de letra
                 let scale = CGAffineTransform(scaleX: 0.7, y: 0.7)
-                let translate = CGAffineTransform(translationX: -10, y: 0)
-                self.placeHolder.transform = translate.concatenating(scale) // scale.concatenating(translate)
+                let translate = CGAffineTransform(translationX: -50, y: 0)
+                self.placeHolder.transform = scale.concatenating(translate)
             }
         } else {
             
