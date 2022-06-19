f610b0b67fb552e99262d76e710407324c1fd8cc
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-06-18 22:59:59 -0500

    base variables and methods to initialize control.

--- Sources/CustomControlsLibrary/RatingControl.swift
+++ Sources/CustomControlsLibrary/RatingControl.swift
@@ -8,15 +8,34 @@
 import UIKit
 
 class RatingControl: UIControl {
-
     
+    lazy var contentStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    let quantity: Int
+    
+    init(quantity: Int) {
+        self.quantity = quantity
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        
+    }
     
     /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
 }
