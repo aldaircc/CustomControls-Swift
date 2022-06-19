e8d1d53c53d31074bc515e482da5d2b29362ed67
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-06-18 22:56:57 -0500

    base files for rating control.

---
+++ Sources/CustomControlsLibrary/RatingControl.swift
@@ -0,0 +1,22 @@
+//
+//  RatingControl.swift
+//  
+//
+//  Created by Aldair Cosetito Coral on 18/06/22.
+//
+
+import UIKit
+
+class RatingControl: UIControl {
+
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
