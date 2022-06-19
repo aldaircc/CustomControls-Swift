840d4a3ac0e29433c80a2b902753240aea535e1e
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-06-19 16:30:29 -0500

    Declare action event for ratingControl when it changes his value.

--- Sources/CustomControlsLibrary/RatingControl.swift
+++ Sources/CustomControlsLibrary/RatingControl.swift
@@ -7,19 +7,27 @@
 
 import UIKit
 
+@available(iOS 13.0, *)
 class RatingControl: UIControl {
     
     lazy var contentStackView: UIStackView = {
         let stack = UIStackView()
         stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.alignment = .center
+        stack.distribution = .equalSpacing
+        stack.spacing = 5
+        stack.axis = .horizontal
         return stack
     }()
     
-    let quantity: Int
+    let size: Int
+    private(set) var value: Int
     
-    init(quantity: Int) {
-        self.quantity = quantity
+    init(size: Int, value: Int) {
+        self.size = size
+        self.value = value
         super.init(frame: .zero)
+        setupView()
     }
     
     required init?(coder: NSCoder) {
@@ -29,13 +37,24 @@ class RatingControl: UIControl {
     func setupView() {
         addSubview(contentStackView)
         
-        for index in 0..<quantity {
-            contentStackView.addArrangedSubview(generateView(id: index))
+        for index in 0..<size {
+            contentStackView.addArrangedSubview(generateView(id: index, selected: false))
         }
     }
     
-    func generateView(id: Int) -> UIView {
-        return UIView()
+    func generateView(id: Int, selected: Bool) -> UIImageView {
+        if let image = UIImage(systemName: !selected ? "star" : "star.fill") {
+            image.withTintColor(selected ? .yellow : .clear)
+            let imageView = UIImageView(image: image)
+            imageView.tag = id
+            return imageView
+        }
+        
+        return UIImageView()
+    }
+    
+    func ratingSelectionChanging(_ sender: UIImageView) {
+        sendActions(for: .valueChanged)
     }
     
     /*
--- Tests/CustomControlsLibraryTests/RatingControlTests.swift
+++ Tests/CustomControlsLibraryTests/RatingControlTests.swift
@@ -6,30 +6,34 @@
 //
 
 import XCTest
+@testable import CustomControlsLibrary
 
+@available(iOS 13.0, *)
 class RatingControlTests: XCTestCase {
 
+    var ratingControl: RatingControl?
+    let size = 5
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        ratingControl = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testCreateRatingControl() throws {
+        ratingControl = RatingControl(size: size, value: 0)
+        XCTAssertNotNil(ratingControl)
+        let container = try XCTUnwrap(ratingControl?.contentStackView.arrangedSubviews)
+        XCTAssertEqual(container.count, size)
+        //XCTAssert(<#T##expression: Bool##Bool#>, <#T##message: String##String#>)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testGetValue() throws {
+        ratingControl = RatingControl(size: size, value: 2)
+        let value = try XCTUnwrap(ratingControl?.value)
+        
     }
-
 }
